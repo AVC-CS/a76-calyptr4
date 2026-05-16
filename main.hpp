4d59e1760fd045d26c82c7d7e8220333ed7570e3
calyptr4 <alessandrabaires@gmail.com>
2026-05-15 17:45:41 -0700

    redundancy fixed

--- main.hpp
+++ main.hpp
@@ -40,7 +40,6 @@ int makeNameRecord(string state[], string gender[], int year[], string name[], i
 int findNames(int cnt, string state[], string gender[], int year[], string name[], int count[], char starting, string stname)
 {
     for(int i=0; i<cnt; i++){
-        if(name[i].rfind(starting, 0) == 0)
             if(state[i] == stname && name[i].rfind(starting, 0) == 0){
                 printoutcontents(state[i], gender[i], year[i], name[i], count[i]);
                 cnt++;
